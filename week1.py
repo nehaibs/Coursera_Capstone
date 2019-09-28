7e67bfb57b2ef051e99d975258ce6fff206a556b
nehaibs <55905291+nehaibs@users.noreply.github.com>
2019-09-28 14:01:52 +0800

    Add files via upload
    week1

---
+++ week1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#this notebook is mainly used for capstone projects of the coursera
+
+
+# In[ ]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[6]:
+
+
+print("Hello Capstone Project!!")
+
+
+# In[ ]:
+
+
+
+
